72aacfa1cf2f8ff24058b9cc3f78c7b708c97de6
raptor-2001 <pratham007yadav@gmail.com>
2022-11-14 09:04:10 +0530

    verified text added

--- pages/profile.php
+++ pages/profile.php
@@ -5,12 +5,31 @@
     exit();
   } 
   include_once('./authentication.php');
+  $pdo = require_once('../database.php');
+  $email = $_SESSION['email'];
+  $statement = $pdo->prepare('SELECT email FROM verified_user WHERE email like :email');
+  $statement->bindValue(":email",$email);
+  $statement->execute();
+  
+  $phone_number_verified =  false;
+
+  if($statement->fetch()){
+    $phone_number_verified = true;
+  }else{
+    $phone_number_verified = false;
+  }
+  
+  
 ?>
 
 <?php include_once('../components/loginNav.php')?>
   <div class="container">
+  <?php if(!$phone_number_verified) : ?>
     <form method="post" action="verification.php">
-      <button class="btn btn-success">Verify Phone Number</button>
+        <button class="btn btn-danger">Verify Phone Number</button>
     </form>
+  <?php else : ?>
+    <h4 class="text-success">Phone Number Verified</h4>
+  <?php endif; ?>
   </div>  
 <?php include_once('../components/header.php');?>
